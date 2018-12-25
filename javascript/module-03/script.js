036c0b8f0feae65b5f0e484e90bfd26918351933
Konstantin Chernykh <costaslavich@gmail.com>
2018-12-25 17:01:46 +0200

    hw03/3

--- javascript/module-03/script.js
+++ javascript/module-03/script.js
@@ -5,30 +5,34 @@ const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 const MESSAGE_INVALID = 'Ошибка! Логин должен быть от 4 до 16 символов';
 const MESSAGE_LOGIN_ADDED = 'Логин успешно добавлен!';
 const MESSAGE_LOGIN_FALSE = 'Такой логин уже используется!';
+const MESSAGE_CANCELED = 'Нажата отмена!';
 
 const login = prompt('Введите логин!');
 
 const isLoginValid = function(login) {
-  if (!login) return;
-  else if (login.length < 4 || login.length > 16) {
+  if (login.length < 4 || login.length > 16) {
     console.log(MESSAGE_INVALID);
-  }
+    return false;
+  } else return true;
 };
 
 const isLoginUnique = function(allLogins, login) {
-  for (const value of allLogins) {
-    if (value === login) console.log(MESSAGE_LOGIN_FALSE);
-  }
+  if (logins.includes(login)) {
+    console.log(MESSAGE_LOGIN_FALSE);
+    return false;
+  } else return true;
 };
 
 const addLogin = function(allLogins, login) {
-  isLoginValid(login);
-  isLoginUnique(logins, login);
-  if (isLoginValid() && isLoginUnique()) {
+  if (!login) {
+    return console.log(MESSAGE_CANCELED);
+  }
+  if (isLoginValid(login) && isLoginUnique(logins, login)) {
     console.log(MESSAGE_LOGIN_ADDED);
-    logins.push(login);
+    return logins.push(login);
   }
 };
 
 addLogin(logins, login);
+
 console.log(logins);
