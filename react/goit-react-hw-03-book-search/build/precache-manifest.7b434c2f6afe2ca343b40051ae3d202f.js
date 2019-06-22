b90e7785ebf1852714cf927e6391799fa0ffcfb8
Konstantin Chernykh <costaslavich@gmail.com>
2019-06-23 00:59:32 +0300

    hw/03-book-search/2

---
+++ react/goit-react-hw-03-book-search/build/precache-manifest.7b434c2f6afe2ca343b40051ae3d202f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c8deed53e7774a51fadbd0cffd384c9",
+    "url": "/goit-fe-course/index.html"
+  },
+  {
+    "revision": "4b3430b16f15f69ede59",
+    "url": "/goit-fe-course/static/css/main.16f415f1.chunk.css"
+  },
+  {
+    "revision": "c008b51c0b6e7fdaf6a2",
+    "url": "/goit-fe-course/static/js/2.e6b7a691.chunk.js"
+  },
+  {
+    "revision": "4b3430b16f15f69ede59",
+    "url": "/goit-fe-course/static/js/main.61537b1d.chunk.js"
+  },
+  {
+    "revision": "6fc20f7e957d07e7940e",
+    "url": "/goit-fe-course/static/js/runtime~main.e6ce5aa7.js"
+  }
+]);
