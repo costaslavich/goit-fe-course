acbc78472b4362e5588fec8a0258fecb666428da
Konstantin Chernykh <costaslavich@gmail.com>
2019-06-23 19:02:01 +0300

    hw/03-book-search/6

--- react/goit-react-hw-03-book-search/build/service-worker.js
+++ react/goit-react-hw-03-book-search/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-fe-course/goit-react-hw-03-book-search/precache-manifest.a4b7822fb5589abb0b8a1f358945dba0.js"
+  "/goit-fe-course/goit-react-hw-03-book-search/build/precache-manifest.4cf36432041f875a09ee07c7f756e422.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-fe-course/goit-react-hw-03-book-search/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-fe-course/goit-react-hw-03-book-search/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
